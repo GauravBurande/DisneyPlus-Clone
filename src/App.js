83fbbea05deb5dc836f5a2a802d3db879754ba6f
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-01 20:03:47 +0530

    created and completed nav and navmenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route exact path='/' element={<Login />}>
           </Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt="disney+" />
+            </Logo>
+            <NavMenu>
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt="home" />
+                    <span>HOME</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/search-icon.svg" alt="home" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/watchlist-icon.svg" alt="watchlist" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/original-icon.svg" alt="originals" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/movie-icon.svg" alt="movies" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/series-icon.svg" alt="series" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: 3;
+`
+
+const Logo = styled.a`
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    display: inline-block;
+    img {
+        display: block;
+        width: 100%;
+    }
+`
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    margin-left: 25px;
+    margin-right: auto;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            width: 20px;
+            max-height: 20px;
+            height: 20px;
+            z-index: auto;
+        }
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08px;
+            padding: 2px 5px;
+            white-space: nowrap;
+            position: relative;
+
+            ::before {
+            background-color: rgb(249, 249, 249);
+            border-radius: 0 0 4px 4px;
+            bottom: -10px;
+            content: '';
+            height: 2px;
+            left: 0px;
+            opacity: 0;
+            position: absolute;
+            right: 0;
+            transform-origin: left center;
+            transform: scaleX(0);
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            visibility: hidden;
+            width: auto;
+            }
+        }
+
+        :hover {
+            span::before {
+            transform: scaleX(1);
+            visibility: visible;
+            opacity: 1 !important;
+            }
+        }
+
+
+    }
+
+    /* @media (max-width: 768px) {
+        display: none;
+    } */
+`
+
+
+
+export default Header
