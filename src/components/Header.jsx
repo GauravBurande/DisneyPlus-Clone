a4c775c5a4dc6b8bb3bd36940fd3a84587aacfcf
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-05 13:59:25 +0530

    fixed 403 error for profile image with help of stackoverflow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -83,7 +83,7 @@ const Header = () => {
                             </a>
                         </NavMenu>
                         <SignOut>
-                            <UserImg src={userPhoto} alt={userName} />
+                            <UserImg src={userPhoto} alt={userName} referrerPolicy="no-referrer" />
                             <DropDown>
                                 <span onClick={handleAuth}>Sign Out</span>
                             </DropDown>
